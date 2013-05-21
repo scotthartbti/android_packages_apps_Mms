c1a3eae17811b7ac028fb93fe015b5fcd3f507f3
Muhammed Nazim <mnm9994u@gmail.com>
2013-05-21 23:52:20 +0200

    Enhance the Easy Selector
    - Follow GoogleUI Guidelines
    - Fullscreen selector
    - Added groups support
    
    Credits to original author Eric Mourougandy
    
    Change-Id: Iea1757585f337052cda02e40f5afa321fdc1730d
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	src/com/android/mms/ui/ComposeMessageActivity.java

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -34,6 +34,7 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -166,7 +167,6 @@ import android.widget.Toast;
 
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
-import com.android.internal.util.CharSequences;
 import com.android.mms.LogTag;
 import com.android.mms.MmsApp;
 import com.android.mms.MmsConfig;
@@ -233,6 +233,7 @@ public class ComposeMessageActivity extends Activity
     public static final int REQUEST_CODE_ADD_CONTACT      = 108;
     public static final int REQUEST_CODE_PICK             = 109;
     public static final int REQUEST_CODE_INSERT_CONTACT_INFO = 110;
+    public static final int REQUEST_CODE_ADD_RECIPIENTS   = 111;
 
     private static final String TAG = "Mms/compose";
 
@@ -339,7 +340,7 @@ public class ComposeMessageActivity extends Activity
 
     private RecipientsEditor mRecipientsEditor;  // UI control for editing recipients
     private ImageButton mRecipientsPicker;       // UI control for recipients picker
-    private ImageButton mRecipientsSelector;       // UI control for recipients selector
+    private ImageButton mRecipientsSelector;     // UI control for recipients selector
 
     private boolean mIsKeyboardOpen;             // Whether any keyboard is open
     private boolean mIsHardKeyboardOpen;         // Whether the hard keyboard is open
@@ -1976,11 +1977,13 @@ public class ComposeMessageActivity extends Activity
             mRecipientsEditor = (RecipientsEditor) stubView.findViewById(R.id.recipients_editor);
             mRecipientsPicker = (ImageButton) stubView.findViewById(R.id.recipients_picker);
             mRecipientsSelector = (ImageButton) stubView.findViewById(R.id.recipients_selector);
+            mRecipientsSelector.setVisibility(View.VISIBLE);
         } else {
             mRecipientsEditor = (RecipientsEditor)findViewById(R.id.recipients_editor);
             mRecipientsEditor.setVisibility(View.VISIBLE);
             mRecipientsPicker = (ImageButton)findViewById(R.id.recipients_picker);
             mRecipientsSelector = (ImageButton)findViewById(R.id.recipients_selector);
+            mRecipientsSelector.setVisibility(View.VISIBLE);
         }
         mRecipientsPicker.setOnClickListener(this);
         mRecipientsSelector.setOnClickListener(this);
@@ -2099,7 +2102,6 @@ public class ComposeMessageActivity extends Activity
         mBackgroundQueryHandler = new BackgroundQueryHandler(mContentResolver);
 
         initialize(savedInstanceState, 0);
-        updateEasySelector();
 
         if (TRACE) {
             android.os.Debug.startMethodTracing("compose");
@@ -2579,7 +2581,6 @@ public class ComposeMessageActivity extends Activity
                                 InputType.TYPE_TEXT_FLAG_MULTI_LINE);
 
         setBackground(); // custom background
-        updateEasySelector();
     }
 
     @Override
@@ -3359,12 +3360,21 @@ public class ComposeMessageActivity extends Activity
 
             case REQUEST_CODE_INSERT_CONTACT_INFO:
                 showContactInfoDialog(data.getData());
+		break;
+
+            case REQUEST_CODE_ADD_RECIPIENTS:
+                insertNumbersIntoRecipientsEditor((String[])data.getExtra("com.android.mms.ui.AddRecipients"));
                 break;
 
             default:
                 if (LogTag.VERBOSE) log("bail due to unknown requestCode=" + requestCode);
                 break;
         }
+    }
+
+    private void insertNumbersIntoRecipientsEditor(String[] numbers) {
+        ContactList list = ContactList.getByNumbers(Arrays.asList(numbers), true);
+        mRecipientsEditor.populate(list);
     }
 
     private void processPickResult(final Intent data) {
@@ -3735,11 +3745,11 @@ public class ComposeMessageActivity extends Activity
     public void onClick(View v) {
         if ((v == mSendButtonSms || v == mSendButtonMms) && isPreparedForSending()) {
             confirmSendMessageIfNeeded();
-        } else if ((v == mRecipientsPicker)) {
+        } else if (v == mRecipientsPicker) {
             launchMultiplePhonePicker();
-        } else if ((v == mRecipientsSelector)) {
-            //Toast.makeText(getApplicationContext(), "click sur selecteur", Toast.LENGTH_LONG).show();
-            launchRecipientsSelector();
+        } else if (v == mRecipientsSelector) {
+            Intent intent = new Intent(ComposeMessageActivity.this, AddRecipientsList.class);
+            startActivityForResult(intent, REQUEST_CODE_ADD_RECIPIENTS);
         }
     }
 
@@ -3761,100 +3771,6 @@ public class ComposeMessageActivity extends Activity
             intent.putExtra(Intents.EXTRA_PHONE_URIS, uris);
         }
         startActivityForResult(intent, REQUEST_CODE_PICK);
-    }
-
-    private ArrayList<CharSequence[]> getContactsNumbersInfo() {
-        final String[] projection = new String[] {
-                Phone.NUMBER,
-                Phone.TYPE,
-                Phone.LABEL,
-                Phone.DISPLAY_NAME,
-                Phone.IS_PRIMARY
-        };
-        final String where = Phone.NUMBER + " NOT NULL";
-        final String orderBy = Phone.DISPLAY_NAME + ", "
-              + "CASE WHEN " + Phone.IS_PRIMARY + " = 0 THEN 1 ELSE 0 END";
-
-        final Cursor cursor = getContentResolver().query(Phone.CONTENT_URI,
-                projection, where, null, orderBy);
-
-        if (cursor == null) {
-            return null;
-        }
-
-        final int count = cursor.getCount();
-
-        if (count == 0) {
-            cursor.close();
-            return null;
-        }
-
-        final ArrayList<CharSequence[]> items = new ArrayList<CharSequence[]>(count);
-
-        for (int i = 0; i < count; i++) {
-            cursor.moveToPosition(i);
-
-            String number = cursor.getString(0);
-            int type = cursor.getInt(1);
-            String label = cursor.getString(2);
-            String name = cursor.getString(3);
-
-            items.add(i, new CharSequence[] {name, Phone.getTypeLabel(getResources(), type, label), number});
-        }
-
-        cursor.close();
-
-        return items;
-    }
-
-    private void launchRecipientsSelector() {
-        final int displayNameColumn = 0;
-        final int labelColumn = 1;
-        final int numberColumn = 2;
-
-        final ArrayList<CharSequence[]> data = getContactsNumbersInfo();
-
-        if (data == null) {
-            return;
-        }
-
-        final int count = data.size();
-        final CharSequence[] entries = new CharSequence[count];
-        for (int i = 0; i < count; i++) {
-            entries[i] = data.get(i)[displayNameColumn] + " - " + data.get(i)[labelColumn]
-                    + "\n" + data.get(i)[numberColumn];
-        }
-
-        final boolean[] numbersChecked = new boolean[entries.length];
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setIcon(R.drawable.ic_contact_picture);
-        builder.setTitle(R.string.add_recipients);
-
-        builder.setMultiChoiceItems(entries, null, new DialogInterface.OnMultiChoiceClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                numbersChecked[which] = isChecked;
-            }
-        });
-
-        builder.setPositiveButton(R.string.add_recipients_positive_button,
-                new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                for (int i = 0; i < count; i++) {
-                    if (numbersChecked[i]) {
-                        int start = mRecipientsEditor.getSelectionStart();
-                        int end = mRecipientsEditor.getSelectionEnd();
-                        mRecipientsEditor.getText().replace(
-                                Math.min(start, end), Math.max(start, end), data.get(i)[numberColumn] + ",");
-                    }
-                }
-            }
-        });
-        builder.setNegativeButton(android.R.string.cancel, null);
-
-        builder.show();
     }
 
     @Override
@@ -3999,6 +3915,15 @@ public class ComposeMessageActivity extends Activity
         mAttachmentEditor = (AttachmentEditor) findViewById(R.id.attachment_editor);
         mAttachmentEditor.setHandler(mAttachmentEditorHandler);
         mAttachmentEditorScrollView = findViewById(R.id.attachment_editor_scroll_view);
+        // Toggle Easy Selector
+        LinearLayout mEasySelector = (LinearLayout) findViewById(R.id.button_multi_selection);
+        if (mEasySelector != null) {
+            if (prefs.getBoolean("pref_easy_selector", false)) {
+                mEasySelector.setVisibility(View.VISIBLE);
+            } else {
+                mEasySelector.setVisibility(View.GONE);
+            }
+        }
     }
 
     private void confirmDeleteDialog(OnClickListener listener, boolean locked) {
@@ -5253,17 +5178,5 @@ public class ComposeMessageActivity extends Activity
                 return builder.create();
         }
         return super.onCreateDialog(id, args);
-    }
-
-    void updateEasySelector() {
-        SharedPreferences prefs = PreferenceManager
-                .getDefaultSharedPreferences((Context) ComposeMessageActivity.this);
-        LinearLayout mEasySelector = (LinearLayout) findViewById(R.id.button_multi_selection);
-        if (mEasySelector == null) return;
-        if (prefs.getBoolean("pref_easy_selector", false) == true) {
-            mEasySelector.setVisibility(View.VISIBLE);
-        } else {
-            mEasySelector.setVisibility(View.GONE);
-        }
     }
 }
