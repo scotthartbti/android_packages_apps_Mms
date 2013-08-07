e5a9e71ecb6180bff530e5cfdfd6b8642da0edd4
scott <scott@ubuntu.com>
2013-08-07 16:22:12 -0400

    Revert "Use custom notification from contact if available."
    This reverts commit fb113cc5d70e7d746b75695e18900e1777c3db96.

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -287,8 +287,7 @@ public class MessagingNotification {
                                 "sCurrentlyDisplayedThreadId so NOT showing notification," +
                                 " but playing soft sound. threadId: " + newMsgThreadId);
                     }
-                    NotificationInfo ni = (NotificationInfo)notificationSet.first();
-                    playInConversationNotificationSound(context, ni.mSender.getCustomNotification());
+                    playInConversationNotificationSound(context);
                     return;
                 }
             }
@@ -331,25 +330,6 @@ public class MessagingNotification {
                 player.stop();
             }
         }, 5000);
-    }
-
-    /**
-     * Play the in-conversation notification sound (it's the regular notification sound, but
-     * played at half-volume
-     */
-    private static void playInConversationNotificationSound(Context context,
-            String customNotification) {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        String ringtoneStr = customNotification == null ? sp.getString(MessagingPreferenceActivity.NOTIFICATION_RINGTONE,
-                null) : customNotification;
-        if (TextUtils.isEmpty(ringtoneStr)) {
-            // Nothing to play
-            return;
-        }
-        Uri ringtoneUri = Uri.parse(ringtoneStr);
-        NotificationPlayer player = new NotificationPlayer(LogTag.APP);
-        player.play(context, ringtoneUri, false, AudioManager.STREAM_NOTIFICATION,
-                IN_CONVERSATION_NOTIFICATION_VOLUME);
     }
 
     /**
@@ -932,8 +912,6 @@ public class MessagingNotification {
         String title = null;
         String privateModeContentText = null;
         Bitmap avatar = null;
-	String customNotification = null;
-
         if (uniqueThreadCount > 1) {    // messages from multiple threads
             Intent mainActivityIntent = new Intent(Intent.ACTION_MAIN);
 
@@ -952,7 +930,6 @@ public class MessagingNotification {
         } else {    // same thread, single or multiple messages
             if (!privacyMode) {
                 title = mostRecentNotification.mTitle;
-		customNotification = mostRecentNotification.mSender.getCustomNotification();
                 BitmapDrawable contactDrawable = (BitmapDrawable)mostRecentNotification.mSender
                         .getAvatar(context, null);
                 if (contactDrawable != null) {
@@ -1049,8 +1026,8 @@ public class MessagingNotification {
                 }
             }
 
-            String ringtoneStr = customNotification == null ? sp.getString(MessagingPreferenceActivity.NOTIFICATION_RINGTONE,
-                    null) : customNotification;
+            String ringtoneStr = sp.getString(MessagingPreferenceActivity.NOTIFICATION_RINGTONE,
+                    null);
             noti.setSound(TextUtils.isEmpty(ringtoneStr) ? null : Uri.parse(ringtoneStr));
             if (DEBUG) {
                 Log.d(TAG, "updateNotification: new message, adding sound to the notification");
