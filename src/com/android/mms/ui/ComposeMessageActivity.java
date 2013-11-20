f04b531e50642a7fba2c72532e0f4705fe14d8fe
Scott <scott@ubuntu>
2013-11-20 01:13:30 -0500

    Revert "Mms: Add the ability to select recipients from a checkbox list"
    This reverts commit a25f023b9d8c6ad582021e1b3420c0090968d4e1.

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -34,7 +34,6 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -199,7 +198,6 @@ public class ComposeMessageActivity extends Activity
     public static final int REQUEST_CODE_ECM_EXIT_DIALOG  = 107;
     public static final int REQUEST_CODE_ADD_CONTACT      = 108;
     public static final int REQUEST_CODE_PICK             = 109;
-    public static final int REQUEST_CODE_ADD_RECIPIENTS   = 111;
 
     private static final String TAG = "Mms/compose";
 
@@ -310,7 +308,6 @@ public class ComposeMessageActivity extends Activity
 
     private RecipientsEditor mRecipientsEditor;  // UI control for editing recipients
     private ImageButton mRecipientsPicker;       // UI control for recipients picker
-    private ImageButton mRecipientsSelector;     // UI control for recipients selector
 
     // For HW keyboard, 'mIsKeyboardOpen' indicates if the HW keyboard is open.
     // For SW keyboard, 'mIsKeyboardOpen' should always be true.
@@ -1854,17 +1851,12 @@ public class ComposeMessageActivity extends Activity
             View stubView = stub.inflate();
             mRecipientsEditor = (RecipientsEditor) stubView.findViewById(R.id.recipients_editor);
             mRecipientsPicker = (ImageButton) stubView.findViewById(R.id.recipients_picker);
-            mRecipientsSelector = (ImageButton) stubView.findViewById(R.id.recipients_selector);
-            mRecipientsSelector.setVisibility(View.VISIBLE);
         } else {
             mRecipientsEditor = (RecipientsEditor)findViewById(R.id.recipients_editor);
             mRecipientsEditor.setVisibility(View.VISIBLE);
             mRecipientsPicker = (ImageButton)findViewById(R.id.recipients_picker);
-            mRecipientsSelector = (ImageButton)findViewById(R.id.recipients_selector);
-            mRecipientsSelector.setVisibility(View.VISIBLE);
         }
         mRecipientsPicker.setOnClickListener(this);
-        mRecipientsSelector.setOnClickListener(this);
 
         mRecipientsEditor.setAdapter(new ChipsRecipientAdapter(this));
         mRecipientsEditor.populate(recipients);
@@ -3197,27 +3189,10 @@ public class ComposeMessageActivity extends Activity
                 }
                 break;
 
-            case REQUEST_CODE_ADD_RECIPIENTS:
-                insertNumbersIntoRecipientsEditor(
-                        data.getStringArrayListExtra(SelectRecipientsList.EXTRA_RECIPIENTS));
-                break;
-
             default:
                 if (LogTag.VERBOSE) log("bail due to unknown requestCode=" + requestCode);
                 break;
         }
-    }
-
-    private void insertNumbersIntoRecipientsEditor(final ArrayList<String> numbers) {
-        ContactList list = ContactList.getByNumbers(numbers, true);
-        ContactList existing = mRecipientsEditor.constructContactsFromInput(true);
-        for (Contact contact : existing) {
-            if (!contact.existsInDatabase()) {
-                list.add(contact);
-            }
-        }
-        mRecipientsEditor.setText(null);
-        mRecipientsEditor.populate(list);
     }
 
     private void processPickResult(final Intent data) {
@@ -3574,13 +3549,8 @@ public class ComposeMessageActivity extends Activity
     public void onClick(View v) {
         if ((v == mSendButtonSms || v == mSendButtonMms) && isPreparedForSending()) {
             confirmSendMessageIfNeeded();
-        } else if (v == mRecipientsPicker) {
+        } else if ((v == mRecipientsPicker)) {
             launchMultiplePhonePicker();
-        } else if (v == mRecipientsSelector) {
-            Intent intent = new Intent(ComposeMessageActivity.this, SelectRecipientsList.class);
-            ContactList contacts = mRecipientsEditor.constructContactsFromInput(false);
-            intent.putExtra(SelectRecipientsList.EXTRA_RECIPIENTS, contacts.getNumbers());
-            startActivityForResult(intent, REQUEST_CODE_ADD_RECIPIENTS);
         }
     }
 
