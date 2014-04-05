5602fbaf5fec5b30094647e5e535ee3f0d6941a4
Valerio Pilo <vpilo@coldshock.net>
2014-04-05 18:51:26 +0200

    QuickMessagePopup: pager fix on multiple messages.
    Fixed the pager to
    * keep the already typed replies for each message, and
    * retain the typing focus on the current message reply's EditText.
    
    Change-Id: I48bc7e5c597dd31ae13a6ce81f33f870f9ec2be8

--- src/com/android/mms/quickmessage/QuickMessagePopup.java
+++ src/com/android/mms/quickmessage/QuickMessagePopup.java
@@ -784,6 +784,10 @@ public class QuickMessagePopup extends Activity implements
                     Log.d(LOG_TAG, "instantiateItem(): Creating page #" + (position + 1) + " for message from "
                             + qm.getFromName() + ". Number of pages to create = " + getCount());
 
+                if (mCurrentQm == null) {
+                    mCurrentQm = qm;
+                }
+
                 // Set the general fields
                 qmFromName.setText(qm.getFromName());
                 qmTimestamp.setText(MessageUtils.formatTimeStampString(mContext, qm.getTimestamp(), mFullTimestamp));
@@ -952,7 +956,12 @@ public class QuickMessagePopup extends Activity implements
         }
 
         @Override
-        public void finishUpdate(View arg0) {}
+        public void finishUpdate(View arg0) {
+            if (mCurrentQm != null && mCurrentQm.getEditText() != null) {
+                // After a page switch, re-focus on the reply editor
+                mCurrentQm.getEditText().requestFocus();
+            }
+        }
 
         @Override
         public void restoreState(Parcelable arg0, ClassLoader arg1) {}
@@ -963,7 +972,12 @@ public class QuickMessagePopup extends Activity implements
         }
 
         @Override
-        public void startUpdate(View arg0) {}
+        public void startUpdate(View arg0) {
+            if (mCurrentQm != null) {
+                // When the view is refreshed, preserve the current reply
+                mCurrentQm.saveReplyText();
+            }
+        }
 
         @Override
         public void onPageScrollStateChanged(int arg0) {}
