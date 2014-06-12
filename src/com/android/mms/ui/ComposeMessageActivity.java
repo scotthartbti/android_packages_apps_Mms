bc977cb776cb301116e492d121a19adb416a6756
Shuhao Wu <shuhao@shuhaowu.com>
2014-06-12 18:20:37 -0400

    Per conversation notification settings/ringtones
    Added per conversation options. Right now the only options for each
    conversation is custom notification ringtone, vibration settings.
    
    This is useful as for each thread, you can have a custom ringtone.
    
    The implementation is done with a custom database. This is ideal as we
    don't want to modify sqlite database from google as if they upgrade, we
    will run into a lot of trouble.
    
    Github branch:
    https://github.com/shuhaowu/android_packages_apps_Mms/tree/per-thread-options
    
    PS4: Minor changes from comments.
    
    Change-Id: I8e8e533cdcda14b3bc6adea0bb0db04cfbf0ce2b
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	res/values/bs_strings.xml

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -257,6 +257,7 @@ public class ComposeMessageActivity extends Activity
     private static final int MENU_ADD_TEMPLATE          = 34;
     private static final int MENU_ADD_TO_BLACKLIST      = 35;
     private static final int MENU_ADD_TO_CALENDAR       = 36;
+    private static final int MENU_CONVERSATION_OPTIONS  = 37;
 
     private static final int DIALOG_TEMPLATE_SELECT     = 1;
     private static final int DIALOG_TEMPLATE_NOT_AVAILABLE = 2;
@@ -2887,6 +2888,10 @@ public class ComposeMessageActivity extends Activity
             }
         }
 
+        if (mConversation.getThreadId() > 0) {
+            menu.add(0, MENU_CONVERSATION_OPTIONS, 0, R.string.menu_conversation_options);
+        }
+
         menu.add(0, MENU_PREFERENCES, 0, R.string.menu_preferences).setIcon(
                 android.R.drawable.ic_menu_preferences);
 
@@ -2980,6 +2985,12 @@ public class ComposeMessageActivity extends Activity
                 mAddContactIntent = item.getIntent();
                 startActivityForResult(mAddContactIntent, REQUEST_CODE_ADD_CONTACT);
                 break;
+            case MENU_CONVERSATION_OPTIONS: {
+                Intent intent = new Intent(this, ConversationOptionsActivity.class);
+                intent.putExtra(THREAD_ID, mConversation.getThreadId());
+                startActivityIfNeeded(intent, -1);
+                break;
+            }
             case MENU_PREFERENCES: {
                 Intent intent = new Intent(this, MessagingPreferenceActivity.class);
                 startActivityIfNeeded(intent, -1);
